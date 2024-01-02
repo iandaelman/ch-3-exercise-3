528854f26854bee4c35f3b37821de1eea3c10768
Ian Daelman <ian.daelman@student.hogent.be>
2024-01-02 11:33:42 +0100

    Hand class implemented

--- src/Domain/Hand.cs
+++ src/Domain/Hand.cs
@@ -2,5 +2,57 @@
 
 public class Hand
     {
+
+
+    private IList<BlackJackCard> cards;
+
+    public IEnumerable<BlackJackCard> Cards => cards;
+    public int NrOfCards => cards.Count;
+    public int Value => CalcualteValue();
+
+    public Hand()
+        {
+        cards = new List<BlackJackCard>();
+        }
+
+    public void AddCard(BlackJackCard card)
+        {
+        cards.Add(card);
+
+        }
+    public void TurnAllCardsFaceUp()
+        {
+        foreach (BlackJackCard card in cards)
+            {
+            if (!card.FaceUp)
+                {
+                card.TurnCard();
+                }
+            }
+        }
+
+    private int CalcualteValue()
+        {
+        int value = 0;
+        bool hasAce = false;
+        foreach (BlackJackCard card in Cards)
+            {
+            value += card.Value;
+
+            if (card.FaceUp && card.FaceValue == FaceValue.Ace)
+                {
+                hasAce = true;
+                }
+
+
+            }
+
+        if (hasAce && (value + 10) <= 21)
+            {
+            value += 10;
+            }
+
+        return value;
+        }
     }
 
